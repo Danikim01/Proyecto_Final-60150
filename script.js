4f918f0249debe09a7a57bbec027cb7197f0d415
Danikim01 <dakim@fi.uba.ar>
2023-11-03 01:22:47 -0300

    falta mostrar carrito

--- script.js
+++ script.js
@@ -1,59 +1,56 @@
-let carrito_hashmap = {}
+const contenedorProductos = document.getElementById('product-container')
 
-let buttons = document.querySelectorAll("button.add_product_btn")
+const cuenta_carrito = document.getElementById('cuenta-carrito')
 
-buttons.forEach(button => {
-    const name = button.getAttribute('data-product')
-    carrito_hashmap[name] = 0
-})
+function actualizarCuentaCarrito(){
+  const memoria = JSON.parse(localStorage.getItem("producto"))
+  const cuenta = memoria ? memoria.reduce((acc, producto) => acc + producto.cantidad, 0) : 0
+  cuenta_carrito.innerHTML = cuenta
+}
 
 
-function updateCartDisplay() {
-    const cartList = document.getElementById('cart-list');
-    cartList.innerHTML = '';
-    for (const productName in carrito_hashmap) {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${productName}: ${carrito_hashmap[productName]}`;
-      if (carrito_hashmap[productName] != 0){
-        cartList.appendChild(listItem);
+function agregarAlCarrito(producto){
+    const memoria = JSON.parse(localStorage.getItem("producto"))
+    console.log(memoria)
+    if(!memoria){
+      const nuevoProducto = producto
+      nuevoProducto.cantidad = 1
+      localStorage.setItem("producto", JSON.stringify([nuevoProducto]))
+    }else{
+      const indiceProducto = memoria.findIndex(p => p.id === producto.id)
+      if(indiceProducto === -1){
+        const nuevoProducto = producto
+        nuevaMemoria = memoria
+        nuevoProducto.cantidad = 1
+        nuevaMemoria.push(nuevoProducto)
+        localStorage.setItem("producto", JSON.stringify(nuevaMemoria))
+      }else{
+        const nuevaMemoria = memoria
+        const productoEncontrado = nuevaMemoria[indiceProducto]
+        productoEncontrado.cantidad += 1
+        nuevaMemoria[indiceProducto] = productoEncontrado
+        localStorage.setItem("producto", JSON.stringify(nuevaMemoria))
       }
     }
-  }
-
-function showCartPopup() {
-    const cartPopup = document.getElementById('cart-popup');
-    cartPopup.style.display = 'block';
+    actualizarCuentaCarrito()
 }
 
-function closeCartPopup() {
-    const cartPopup = document.getElementById('cart-popup');
-    cartPopup.style.display = 'none';
-}
-
-// let display = document.querySelector("#display_products")
-// /*display elements as ul li*/
-// let unordered_list = document.createElement("ul")
-// display.appendChild(unordered_list) 
-
-
-buttons.forEach(button => {
-    button.addEventListener("click", ()=>{
-        const productName = button.getAttribute('data-product')
-        /*set key:value where key is the product name and value is the amount of clicks*/
-        carrito_hashmap[productName] += 1 
-        updateCartDisplay() 
-        /*display the product and its amount in carrito.html*/
-        // let list_item = document.createElement("li")
-        // list_item.innerHTML = productName + ": " + carrito_hashmap[productName]
-        // unordered_list.appendChild(list_item)
-
+function crearProductos(productos){
+  productos.forEach(producto => {
+    const nuevoProducto = document.createElement('div')
+    nuevoProducto.classList = "tarjeta-producto"
+    nuevoProducto.innerHTML = `
+      <img src="${producto.img}" alt="${producto.nombre}">
+      <h3>${producto.nombre}</h3>
+      <p>Precio: $${producto.precio}</p>
+      <button id="btn-agregar">Agregar al carrito</button>
+    `
+    contenedorProductos.appendChild(nuevoProducto)
+    nuevoProducto.querySelector('#btn-agregar').addEventListener('click', () => {
+      agregarAlCarrito(producto)
     })
-})
 
+  })
+}
 
-document.getElementById("carrito_icon").addEventListener("click",showCartPopup)
-document.getElementById("close-cart-popup").addEventListener("click",closeCartPopup)
-
-
-
-
+crearProductos(productos)
