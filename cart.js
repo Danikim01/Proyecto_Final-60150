63dcea87960694c14daf86dad2b2c4d5ae97c0dd
Danikim01 <dakim@fi.uba.ar>
2023-11-04 01:23:51 -0300

    logica carrito  botones implementada

--- cart.js
+++ cart.js
@@ -1,15 +1,35 @@
 const contenedorProductos = document.getElementById('carrito-container')
 
+function actualizarUnidadesCarrito(){
+    const productos = JSON.parse(localStorage.getItem("producto"))
+    const cantidad_total = productos.reduce((acc, producto) => acc + producto.cantidad, 0)
+    document.querySelector("#unidades").innerHTML = cantidad_total
+}
+
+function actualizarPrecioTotal(){
+    const productos = JSON.parse(localStorage.getItem("producto"))
+    const precio_total = productos.reduce((acc, producto) => acc + producto.precio * producto.cantidad, 0)
+    document.querySelector("#precio").innerHTML = precio_total
+}
+
+
+function actualizarCantidad(operacion,producto){
+    const nuevaMemoria = JSON.parse(localStorage.getItem("producto"))
+    const indiceProducto = nuevaMemoria.findIndex(p => p.id === producto.id)
+    const productoEncontrado = nuevaMemoria[indiceProducto]
+    operacion === 'sumar' ? productoEncontrado.cantidad += 1 : productoEncontrado.cantidad -= 1
+    nuevaMemoria[indiceProducto] = productoEncontrado
+    localStorage.setItem("producto", JSON.stringify(nuevaMemoria))
+}
+
+
 function botonSumar(nuevoProducto,producto){
     nuevoProducto.querySelector('#btn-sumar').addEventListener('click', () => {
         nuevoProducto.querySelector('#cantidad').innerHTML = parseInt(nuevoProducto.querySelector('#cantidad').innerHTML) + 1
         document.getElementById('cuenta-carrito').innerHTML = parseInt(document.getElementById('cuenta-carrito').innerHTML) + 1
-        const nuevaMemoria = JSON.parse(localStorage.getItem("producto"))
-        const indiceProducto = nuevaMemoria.findIndex(p => p.id === producto.id)
-        const productoEncontrado = nuevaMemoria[indiceProducto]
-        productoEncontrado.cantidad += 1
-        nuevaMemoria[indiceProducto] = productoEncontrado
-        localStorage.setItem("producto", JSON.stringify(nuevaMemoria))
+        actualizarCantidad('sumar',producto)
+        actualizarUnidadesCarrito()
+        actualizarPrecioTotal()
     })
 }
 
@@ -18,12 +38,9 @@ function botonRestar(nuevoProducto,producto){
         if (parseInt(nuevoProducto.querySelector('#cantidad').innerHTML) > 0 && parseInt(document.getElementById('cuenta-carrito').innerHTML) > 0){
             nuevoProducto.querySelector('#cantidad').innerHTML = parseInt(nuevoProducto.querySelector('#cantidad').innerHTML) - 1
             document.getElementById('cuenta-carrito').innerHTML = parseInt(document.getElementById('cuenta-carrito').innerHTML) - 1
-            const nuevaMemoria = JSON.parse(localStorage.getItem("producto"))
-            const indiceProducto = nuevaMemoria.findIndex(p => p.id === producto.id)
-            const productoEncontrado = nuevaMemoria[indiceProducto]
-            productoEncontrado.cantidad -= 1
-            nuevaMemoria[indiceProducto] = productoEncontrado
-            localStorage.setItem("producto", JSON.stringify(nuevaMemoria))
+            actualizarCantidad('restar',producto)
+            actualizarUnidadesCarrito()
+            actualizarPrecioTotal()
         }
     })
 }
@@ -31,6 +48,8 @@ function botonRestar(nuevoProducto,producto){
 function mostrarCarrito(){
     const productos = JSON.parse(localStorage.getItem("producto"))
     if (productos && productos.length  > 0){
+        contenedorProductos.innerHTML = ""
+        document.getElementById("totales").style.display = "flex"
         productos.forEach(producto => {
             const nuevoProducto = document.createElement('div')
             nuevoProducto.classList = "carrito-producto"
@@ -46,8 +65,22 @@ function mostrarCarrito(){
             `
             botonSumar(nuevoProducto,producto)
             botonRestar(nuevoProducto,producto)
+            actualizarUnidadesCarrito()
+            actualizarPrecioTotal()
             contenedorProductos.appendChild(nuevoProducto)
         })
+    }else{
+        const nuevoProducto = document.createElement('div')
+        nuevoProducto.classList = "carrito-producto"
+        nuevoProducto.innerHTML = `
+        <strong>No hay productos en el carrito</strong>
+        <p>Agregue algun producto al carrito porfavor</p>
+        `
+        nuevoProducto.style.display = "flex"
+        nuevoProducto.style.flexDirection = "column"
+        nuevoProducto.style.height = "70vh" 
+        document.getElementById("totales").style.display = "none"
+        contenedorProductos.appendChild(nuevoProducto)
     }
 }
 
@@ -58,8 +91,6 @@ function actualizarCuentaCarrito(){
   const cuenta = memoria ? memoria.reduce((acc, producto) => acc + producto.cantidad, 0) : 0
   cuenta_carrito.innerHTML = cuenta
 }
-
-
 
 actualizarCuentaCarrito()
 mostrarCarrito()
